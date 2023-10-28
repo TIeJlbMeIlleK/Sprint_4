f3f6070614b089dd8b205f7fb8245ee76e039e0f
TIeJlbMeIlleK <82091306+TIeJlbMeIlleK@users.noreply.github.com>
2023-10-28 13:42:02 +0300

    Изменил порядок вызова кнопок. Добавил некоторые комментарии, для удобства

--- src/test/java/MakeAnOrder.java
+++ src/test/java/MakeAnOrder.java
@@ -19,23 +19,24 @@ public class MakeAnOrder {
     public void makeAnOrder() {
         HomePageLocators homePageLocators = new HomePageLocators(driver);
         OrderPageLocators orderPageLocators = new OrderPageLocators(driver);
-
-        //Делаем заказ по кнопке снизу
         homePageLocators.agreeWithCookie(); //соглашаемся с куки
-        homePageLocators.clickOnBottomButton();
-        orderPageLocators.waitForOrderPage(); //ожидаем загрузки страницы с заказом
-        orderPageLocators.doFullStepsToOrder("Анатолий", "Кульбит", "Ул.Пушкина д.17", "Курская", "80154443001", "30.10.2023", "Черный, обязательно черный!", "black");
-        String actualFirstOrder = driver.findElement(By.xpath(".//div[contains(@class, 'Order_ModalHeader')]")).getText();
-        Assert.assertTrue("Что-то пошло не так!", actualFirstOrder.startsWith("Заказ оформлен"));
 
-        orderPageLocators.clickOnOrderStatus();
-        homePageLocators.returnToHomePage();
-        //Делаем заказ по кнопке сверху
-        homePageLocators.clickOnTopButton();
+        //Делаем заказ по кнопке сверху. Данные передал в метод. Данные уникальны. Это первый набор данных.
+        homePageLocators.clickOnTopButton(); // кликаем на верхнюю кнопку "Заказать"
         orderPageLocators.waitForOrderPage();
         orderPageLocators.doFullStepsToOrder("Владислав", "Евграфов", "Ул.Ленина д.13", "Бульвар Рокоссовского", "85554443423", "14.11.2023", "Серый, обязательно серый!", "grey");
         String actualSecondOrder = driver.findElement(By.xpath(".//div[contains(@class, 'Order_ModalHeader')]")).getText();
         Assert.assertTrue("Что-то пошло не так!", actualSecondOrder.startsWith("Заказ оформлен"));
+
+        orderPageLocators.clickOnOrderStatus();
+        homePageLocators.returnToHomePage();
+
+        //Делаем заказ по кнопке снизу. Данные передал в метод. Данные уникальны. Это второй набор данных.
+        homePageLocators.clickOnBottomButton(); // кликаем на нижнюю кнопку "Заказать"
+        orderPageLocators.waitForOrderPage(); //ожидаем загрузки страницы с заказом
+        orderPageLocators.doFullStepsToOrder("Анатолий", "Кульбит", "Ул.Пушкина д.17", "Курская", "80154443001", "30.10.2023", "Черный, обязательно черный!", "black");
+        String actualFirstOrder = driver.findElement(By.xpath(".//div[contains(@class, 'Order_ModalHeader')]")).getText();
+        Assert.assertTrue("Что-то пошло не так!", actualFirstOrder.startsWith("Заказ оформлен"));
     }
 
     @Before
